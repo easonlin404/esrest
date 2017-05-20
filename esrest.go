df62839944ea44b6e0bfcdd815b5b0b879184c6b
easonlin404 <easonlin404@gmail.com>
2017-05-20 18:09:08 +0800

    add esrest file

---
+++ esrest.go
@@ -0,0 +1,117 @@
+package esrest
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+	"io"
+)
+
+type Builder struct {
+	Url     string
+	Method  string
+	Path    string
+	BodyByte []byte
+	Headers map[string]string
+}
+
+const DefaultContentType = "application/json"
+
+func New() *Builder {
+	return &Builder{
+		Headers: make(map[string]string),
+	}
+}
+
+func (b *Builder) Get(url string) *Builder {
+	b.Url = url
+	b.Method = http.MethodGet
+	return b
+}
+
+func (b *Builder) Post(url string) *Builder {
+	b.Url = url
+	b.Method = http.MethodPost
+	return b
+}
+
+func (b *Builder) Put(url string) *Builder {
+	b.Url = url
+	b.Method = http.MethodPut
+	return b
+}
+
+func (b *Builder) Delete(url string) *Builder {
+	b.Url = url
+	b.Method = http.MethodDelete
+	return b
+}
+
+func (b *Builder) Header(key, value string) *Builder {
+	b.Headers[key] = value
+	return b
+}
+
+func (b *Builder) Body(body []byte) *Builder {
+	b.BodyByte = body
+	return b
+}
+
+func (b *Builder) Do() (*http.Response, error) {
+	if err := b.valid(); err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{Timeout: time.Duration(20 * time.Second)}
+	resp, err := client.Do(b.newRequest())
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	resp.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+	return resp, nil
+}
+
+func (b *Builder) newRequest() *http.Request {
+	var reader io.Reader
+	if len(b.BodyByte) > 0 {
+		reader = bytes.NewBuffer(b.BodyByte)
+	}
+
+	req, _ := http.NewRequest(b.Method, b.Url, reader)
+
+	fmt.Println(req.Header.Get("Content-Type"))
+	if len(req.Header.Get("Content-Type")) == 0 {
+		req.Header.Set("Content-Type", DefaultContentType)
+	}
+
+	for k, v := range b.Headers {
+		req.Header.Set(k, v)
+	}
+
+	return req
+}
+
+func (b *Builder) AsJson(v interface{}) (*http.Response, error) {
+	resp, err := b.Do()
+	if err != nil {
+		return resp, err
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &v)
+	return resp, nil
+}
+
+func (b *Builder) valid() error {
+	if len(b.Url) == 0 {
+		return errors.New("url is empty")
+	}
+	return nil
+}
---
+++ esrest_test.go
@@ -0,0 +1,88 @@
+package esrest
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	n1 := New()
+	n2 := New()
+
+	assert.Equal(t, n1.Headers != nil, true)
+	assert.Equal(t, n1, n2)
+	assert.Equal(t, n1 != nil, true)
+}
+
+func TestSetGet(t *testing.T) {
+	b := New().Get("https://www.google.com")
+	assert.Equal(t, b.Method, http.MethodGet)
+}
+
+func TestSetPost(t *testing.T) {
+	b := New().Post("https://www.google.com")
+	assert.Equal(t, b.Method, http.MethodPost)
+}
+
+func TestSetPut(t *testing.T) {
+	b := New().Put("https://www.google.com")
+	assert.Equal(t, b.Method, http.MethodPut)
+}
+
+func TestSetDelete(t *testing.T) {
+	b := New().Delete("https://www.google.com")
+	assert.Equal(t, b.Method, http.MethodDelete)
+}
+
+func TestSetHeader(t *testing.T) {
+	b := New().Header("key", "value")
+	value := b.Headers["key"]
+	assert.Equal(t, "value", value)
+
+	b.Get("https://www.google.com").Do()
+
+}
+
+func TestValidOk(t *testing.T) {
+	_, err := New().Get("https://www.google.com").Do()
+	assert.Equal(t, nil, err)
+}
+
+func TestValidFail(t *testing.T) {
+	_, err := New().Do()
+	e := errors.New("url is empty")
+	assert.Equal(t, err, e)
+}
+
+func TestDummyUrl(t *testing.T) {
+	_, err := New().Get("http://123").Do()
+	assert.Error(t, err)
+}
+
+func TestAsJsonSuccess(t *testing.T) {
+	type Json struct {
+		Message string `json:"message"`
+	}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json := []byte(`{"message":"hi"}`)
+		w.Write(json)
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(handler))
+	defer server.Close()
+
+	json := &Json{}
+
+	New().Get(server.URL).AsJson(json)
+	assert.Equal(t, json.Message, "hi")
+}
+
+func TestAsJsonFail(t *testing.T) {
+	_,err := New().Get("dummy").AsJson(nil)
+	assert.Error(t,err)
+}
